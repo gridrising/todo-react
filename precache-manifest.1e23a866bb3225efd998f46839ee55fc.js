f922b7be3a85c434ea437b1c1704cb21b0331559
gridrising <pecherskiip@mail.ru>
2020-08-10 13:28:50 +0300

    Updates

---
+++ precache-manifest.1e23a866bb3225efd998f46839ee55fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e999abca0a7415e2f9d375bd7abaace",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b5826e34ecc0df1db3fa",
+    "url": "./static/css/main.6ee355c8.chunk.css"
+  },
+  {
+    "revision": "8b83bc14279ea58a5e32",
+    "url": "./static/js/2.27f9e990.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.27f9e990.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5826e34ecc0df1db3fa",
+    "url": "./static/js/main.296e5667.chunk.js"
+  },
+  {
+    "revision": "ca8051aa2fe5b3ef84c3",
+    "url": "./static/js/runtime-main.673723db.js"
+  }
+]);
